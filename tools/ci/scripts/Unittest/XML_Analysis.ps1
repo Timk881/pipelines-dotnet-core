73bc0857fc00d9bb80d90c157f7c1e7bd8d116aa
Timk881 <tim.kohlert@kurtzersa.de>
2024-12-02 10:31:50 +0100

    Update XML_Analysis.ps1

--- tools/ci/scripts/Unittest/XML_Analysis.ps1
+++ tools/ci/scripts/Unittest/XML_Analysis.ps1
@@ -17,7 +17,9 @@ $FilePath="C:\Users\tkohlert\Desktop\Testagent\_work\2\b\TestResults\$Testname.x
     $totalAssertspassed = $xmlData.testsuites.testsuite.properties.property | Where-Object { $_.name -eq "TotalAssertsPassed" } | Select-Object -ExpandProperty value
     #Aborts
     $totalAborted = $xmlData.testsuites.testsuite.properties.property | Where-Object { $_.name -eq "TotalAborted" } | Select-Object -ExpandProperty value
-
+    
+$totalTestcases = [int]($totalTestcases)
+$successfullTestcases = [int]($successfullTestcases)
 Write-Output "TotalTestcases: $totalTestcases"
 Write-Output "SuccessfullTestcases: $succesfullTestcases"
 
