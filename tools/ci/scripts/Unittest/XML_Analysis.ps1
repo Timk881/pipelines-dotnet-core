f4a8b576322bf9060c3b49ddf4dab622448aa4db
Timk881 <tim.kohlert@kurtzersa.de>
2024-12-02 10:02:50 +0100

    Add files via upload

--- tools/ci/scripts/Unittest/XML_Analysis.ps1
+++ tools/ci/scripts/Unittest/XML_Analysis.ps1
@@ -19,27 +19,27 @@ $FilePath="C:\Users\tkohlert\Desktop\Testagent\_work\2\b\TestResults\$Testname.x
     $TotalAborted = $xmlData.testsuites.testsuite.properties.property | Where-Object { $_.name -eq "TotalAborted" } | Select-Object -ExpandProperty value
     
    if ($totalAssertsFailures -gt 0) {
-        Write-Output "Error: There are failed assertions."
+        Write-Output "Error $Testname: There are failed assertions."
         exit -1
     }
 
     if ($skippedTestcases -gt 0) {
-        Write-Output "Error: There are skipped test cases."
+        Write-Output "Error $Testname: There are skipped test cases."
         exit -1
     }
 
     if ($totalAborted -gt 0) {
-        Write-Output "Error: There are aborted tests."
+        Write-Output "Error $Testname: There are aborted tests."
         exit -1
     }
 
     if ($totalTestcases -ne $successfullTestcases) {
-        Write-Output "Error: Not all test cases were successful."
+        Write-Output "Error $Testname: Not all test cases were successful."
         exit -1
     }
 
     if ($totalAsserts -ne $totalAssertsPassed) {
-        Write-Output "Error: Not all assertions passed."
+        Write-Output "Error $Testname: Not all assertions passed."
         exit -1
     }
 
